099264eed62c7a6717e06c2e4c4992bb7434234a
Abayomi Ayodele <samabayomi301@gmail.com>
2022-01-21 15:13:50 +0100

    invoice item UI & empty invoices fallback

---
+++ src/views/InvoiceView.vue
@@ -0,0 +1,21 @@
+<script>
+import { reactive, toRefs } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+    });
+
+    return {
+      ...toRefs(state),
+    };
+  },
+};
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style lang="scss" scoped></style>
